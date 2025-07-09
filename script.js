a50425c99605b1b304dac20b05f7d9b8e00e2c1c
drfwiphub <danielr.flatt@gmail.com>
2025-07-09 16:02:50 +0100

    gallery fixed, contact form revised using emailjs. layout still to be fixed in all screen sizes

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
-const images = document.querySelectorAll('.gallery img');
-const modal = document.getElementById('modal');
-const modalImg = document.getElementById('modalImage');
-const closeBtn = document.getElementById('closeBtn');
+    const images = document.querySelectorAll('.gallery img');
+    const modal = document.getElementById('modal');
+    const modalImg = document.getElementById('modalImage');
+    const closeBtn = document.getElementById('closeBtn');
 
-images.forEach(img => {
-  img.addEventListener('click', () => {
-    modal.classList.add('open');
-    modalImg.src = img.src.replace('/300/200', '/800/600'); // Load bigger image if available
-    modalImg.alt = img.alt;
-  });
-});
+    images.forEach(img => {
+      img.addEventListener('click', () => {
+        modal.classList.add('open');
+        modalImg.src = img.src.replace('/300/200', '/800/600');
+        modalImg.alt = img.alt;
+      });
+    });
 
-closeBtn.onclick = () => {
-  modal.classList.remove('open');
-  modalImg.src = '';
-};
-//back up for mobile
-modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.classList.remove('open');
-    modalImg.src = '';
-  }
-});
+    closeBtn.onclick = () => {
+      modal.classList.remove('open');
+      modalImg.src = '';
+    };
 
-var interacted = false;
-addEventListener('mousemove', () => interacted = true );
-//addEventListener('keypress', () => interacted = true );
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove('open');
+        modalImg.src = '';
+      }
+    });
 
 
-const requestURL = 'https://api.webhookinbox.com/i/UiDgQCIE/in/'
+/*
+    (function(){
+        emailjs.init("ex0WU-sCy44tYoz1N");
+    })();
 
-function sendData() {
-  let formBody = [
-    'name=' + encodeURIComponent(document.getElementById('name').value),
-    'email=' + encodeURIComponent(document.getElementById('email').value),
-    'message=' + encodeURIComponent(document.getElementById('message').value),
-    'job-role=' + encodeURIComponent(document.getElementById('job-role').value),
-  ].join('&')
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-  let xhr = new XMLHttpRequest()
+        emailjs.sendForm('service_qd7p2b8', 'template_onyw254', this)
+            .then(function() {
+                alert('Message sent!');
+            }, function(error) {
+                alert('Failed to send: ' + JSON.stringify(error));
+            });
+    });*/
 
-  xhr.open('POST', requestURL)
-
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-unrelated')
-
-  xhr.onload = function () {
-    if (xhr.status === 200){
-      window.location ='/thank-you-page'
-
-      return
-    }
-    alert('An error has occured')
-  }
-  sxhr.send(formBody)
-}
+            (function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init({
+              publicKey: "ex0WU-sCy44tYoz1N",
+            });
+        })();
+ 
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('service_qd7p2b8', 'template_onyw254', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
